29ec8a6ecf7956786c256ffc0180c0729f3210d7
Neloress <Lukas.kohl1996@googlemail.com>
2023-11-01 14:40:12 +0100

    initial commit

---
+++ SensorFusionLocationTracking/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SensorFusionLocationTracking
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ SensorFusionLocationTracking/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SensorFusionLocationTracking
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
